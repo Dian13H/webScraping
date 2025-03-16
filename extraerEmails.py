0f9052b6051b4c137b607b83daca84eb10cd9bba
Dian13H <148781858+Dian13H@users.noreply.github.com>
2025-03-15 20:37:02 -0600

    Update extraerEmails.py

--- extraerEmails.py
+++ extraerEmails.py
@@ -1,18 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 
-# URL de la página
 url = "http://127.0.0.1:8000/victima.html"
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
 
-# Encontrar todas las etiquetas <a>
 tags = soup.find_all("a")
 
-# Iterar sobre las etiquetas <a> y extraer correos electrónicos de mailto
 for tag in tags:
     if tag.has_attr("href") and tag['href'].startswith('mailto:'):
-        # Extraer el correo electrónico y mostrarlo
-        email = tag['href'][7:]  # Eliminar "mailto:" del inicio
+        email = tag['href'][7:]
         print(email)
 
